9b9328b182d9756744215f4bd304cdf7a9f23a8c
FahadShahzeb <fahad.neslit@gmail.com>
2023-12-14 18:12:52 +0500

    Updates MAIN HEADER SECTION

--- src/components/MainHeadingsSection.tsx
+++ src/components/MainHeadingsSection.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { coincierge, coinspeaker, cointelegraph, cryptonews } from '../assets';
 import Marquee from "react-marquee-slider";
+import { useWindowDimensions } from '../utils';
 
 const MainHeadingSection = () => {
+
+    const { width } = useWindowDimensions()
+    console.log({ width })
     return (
         <div className='w-full '>
             <div className='flex px-8 flex-row gap-2 justify-between h-full py-8'>
-                <Marquee velocity={130} direction='rtl' onFinish={() => { }} onInit={() => { }} resetAfterTries={200} scatterRandomly={false}>
+                <Marquee velocity={width && width < 500 ? 50 : 130} direction='rtl' onFinish={() => { }} onInit={() => { }} resetAfterTries={200} scatterRandomly={false}>
                     <img src={cryptonews} alt={`PNG`} className='h-[35px] sm:h-[47px] w-[140px] sm:w-[200px] mx-12 sm:mx-32 ' />
                     <img src={coincierge} alt={`PNG`} className='h-[35px] sm:h-[47px] w-[140px] sm:w-[200px] mx-12 sm:mx-32' />
                     <img src={coinspeaker} alt={`PNG`} className='h-[35px] sm:h-[47px] w-[140px] sm:w-[200px] mx-12 sm:mx-32' />
